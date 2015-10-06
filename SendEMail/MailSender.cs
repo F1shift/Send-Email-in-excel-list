6a125a582ce5554008285697907ea04117bfae63
F1shift <lif1shift@gmail.com>
2015-10-06 09:36:55 +0800

    2015/10/6

---
+++ SendEMail/MailSender.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace SendEMail
+{
+    public class MailSender
+    {
+        public static bool SendMails(string excelFilePath, SmtpClient Smtp)
+        {
+            _Application excel;
+            //try
+            //{
+                excel = OpenExcelSheet(excelFilePath);
+                _Worksheet sheet = excel.Worksheets[1];
+                int i = 2;
+                String ErrorLog = "";
+                MailMessage aMail = Read1MailFromExcel(sheet, i, (e) => { ErrorLog += e + "\r\n"; }); i++;
+                while (aMail != null)
+                {
+                    try
+                    {
+                        Smtp.Send(aMail);
+                    }
+                    catch
+                    {
+                        ErrorLog += (i-1) + " Sending Error!\r\n";
+                    }
+                    aMail = Read1MailFromExcel(sheet, i, (e) => { ErrorLog += e + "\r\n"; }); i++;
+                }
+                if (!String.IsNullOrEmpty(ErrorLog))
+                {
+                    if (File.Exists("ErrorLog.txt"))
+	                    File.Delete("ErrorLog.txt");
+                    StreamWriter sw = new StreamWriter("ErrorLog.txt");
+                    sw.Write(ErrorLog);
+                    sw.Flush();
+                    sw.Close();
+                    Process nodepad = new Process();
+                    nodepad.StartInfo.FileName = "ErrorLog.txt";
+                    nodepad.Start();
+                }
+                if (excel != null)
+                    excel.Quit();
+                return true;
+            //}
+            //catch
+            //{
+            //    if (excel != null)
+            //        excel.Quit();
+            //    return false;
+            //}
+        }
+
+        public static _Application OpenExcelSheet(string filePath)
+        {
+            //引用Excel Application類別
+            _Application myExcel = null;
+            //引用活頁簿類別 
+            _Workbook myBook = null;
+
+            //開啟一個新的應用程式
+            myExcel = new Microsoft.Office.Interop.Excel.Application();
+
+            //讓Excel文件可見 
+            myExcel.Visible = false;
+
+            myBook = myExcel.Workbooks.Open(filePath, ReadOnly: false);
+
+            return myExcel;
+        }
+
+        public static MailMessage Read1MailFromExcel(_Worksheet excelSheet, int rowIndex, Action<String> ErrorOutput)
+        {
+            if (excelSheet.Cells[rowIndex, 1].Value2 != null)
+            {
+                MailMessage newMail = new MailMessage();
+                //加入寄件者
+                newMail.From = new MailAddress(((String)excelSheet.Cells[rowIndex, 1].Value2).Trim());
+                //加入收件者
+                if (excelSheet.Cells[rowIndex, 2].Value2 != null)
+                {
+                    String[] Tos = ((String)excelSheet.Cells[rowIndex, 2].Value2).Split(',', ';', '\n');
+                    foreach (var to in Tos)
+                    {
+                        MailAddress adress = null;
+                        try
+                        {
+                            adress = new MailAddress(to.Trim());
+                            newMail.To.Add(adress);
+                        }
+                        catch
+                        {
+                            if (ErrorOutput != null)
+                                ErrorOutput(rowIndex + " Something wrong with Mail Adress : " + to);
+                        }
+                    }
+                }
+                //加入副本
+                if (excelSheet.Cells[rowIndex, 3].Value2 != null)
+                {
+                    String[] CCs = ((String)excelSheet.Cells[rowIndex, 3].Value2).Split(',', ';', '\n');
+                    foreach (var to in CCs)
+                    {
+                        MailAddress adress = null;
+                        try
+                        { 
+                            adress = new MailAddress(to.Trim());
+                            newMail.CC.Add(adress);
+                        }
+                        catch
+                        {
+                            if (ErrorOutput != null)
+                                ErrorOutput(rowIndex + " Something wrong with Mail Adress : " + to);
+                        }
+                    }
+                }
+                //加入密件副本
+                if (excelSheet.Cells[rowIndex, 4].Value2 != null)
+                {
+                    String[] BCCs = ((String)excelSheet.Cells[rowIndex, 4].Value2).Split(',', ';', '\n');
+                    foreach (var to in BCCs)
+                    {
+                        MailAddress adress = null;
+                        try
+                        {
+                            adress = new MailAddress(to.Trim());
+                            newMail.Bcc.Add(adress);
+                        }
+                        catch
+                        {
+                            if (ErrorOutput != null)
+                                ErrorOutput(rowIndex + " Something wrong with Mail Adress : " + to);
+                        }
+                    }
+                }
+                //信件標題
+                if (excelSheet.Cells[rowIndex, 5].Value2 != null)
+                {
+                    String Subject = excelSheet.Cells[rowIndex, 5].Value2;
+                    newMail.Subject = Subject;
+                }
+                else
+                    if (ErrorOutput != null)
+                        ErrorOutput(rowIndex + " Subject is null!");
+                //信件內容
+                if (excelSheet.Cells[rowIndex, 6].Value2 != null)
+                {
+                    String Body = excelSheet.Cells[rowIndex, 6].Value2;
+                    newMail.Body = Body;
+                }
+                else
+                    if (ErrorOutput != null)
+                        ErrorOutput(rowIndex + " Body is null!");
+                //附件
+                String[] Attachments = ((String)excelSheet.Cells[rowIndex, 7].Value2).Split(',', ';', '\n');
+                if (excelSheet.Cells[rowIndex, 7].Value2 != null)
+                {
+                    //[] Attachments = ((String)excelSheet.Cells[rowIndex, 7].Value2).Split(',', ';', '\n');
+                    Attachments = (from filepath in Attachments
+                                  select filepath.Trim()).ToArray();
+                    foreach (var fileName in Attachments)
+                        if (File.Exists(@fileName))
+                        {
+                            newMail.Attachments.Add(new Attachment(File.Open(@fileName, FileMode.Open, FileAccess.Read, FileShare.Read), Path.GetFileName(@fileName)));
+                        }
+                        else if (File.Exists(fileName))
+                        {
+                            newMail.Attachments.Add(new Attachment(File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.Read), Path.GetFileName(fileName)));
+                        }
+                        else
+                        {
+                            if (ErrorOutput != null)
+                                ErrorOutput(rowIndex + " File not found : " + fileName + "!");
+                        }
+                }
+                return newMail;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ SendEMail/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace SendEMail
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.OpenFileDialog OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            OpenFileDialog.Filter = "Excel 活頁簿|*.xlsx";
+            if (OpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.tb_ExcelFilePath.Text = OpenFileDialog.FileName;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (this.tb_ExcelFilePath.Text != null)
+            {
+                Properties.Settings.Default.Save();
+
+                using (SmtpClient client = new SmtpClient(
+                    Properties.Settings.Default.smtpServer,
+                    Properties.Settings.Default.smtpPort))//或公司、客戶的smtp_server
+                {
+                    if (!string.IsNullOrEmpty(Properties.Settings.Default.mailAccount) && !string.IsNullOrEmpty(tb_Pwd.Password))//.config有帳密的話
+                    {//寄信要不要帳密？眾說紛紜Orz，分享一下經驗談....
+
+                        //網友阿尼尼:http://www.dotblogs.com.tw/kkc123/archive/2012/06/26/73076.aspx
+                        //※公司內部不用認證,寄到外部信箱要特別認證 Account & Password
+
+                        //自家公司MIS:
+                        //※要看smtp server的設定呀~
+
+                        //結論...
+                        //※程式在客戶那邊執行的話，問客戶，程式在自家公司執行的話，問自家公司MIS，最準確XD
+                        client.Credentials = new NetworkCredential(Properties.Settings.Default.mailAccount, tb_Pwd.Password);//寄信帳密
+                    }
+
+                    bool succeed = MailSender.SendMails(this.tb_ExcelFilePath.Text, client);
+                    if (succeed)
+                    {
+                        MessageBox.Show("Sending Done!");
+                    }
+                }//end using 
+            }
+        }
+
+        
+    }
+}
